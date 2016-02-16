9ec4794b9f4dd73a7ebdeecc1288cfeab276a733
Darksun2010 <749881399@qq.com>
2016-02-16 13:50:56 +0800

    LSH tester updated

--- LSH/testHamMap.cpp
+++ LSH/testHamMap.cpp
@@ -13,11 +13,9 @@
 #include "hamMap.h"
 #include "simHashpp.h"
 
-int main(void){
+int main(int argc,char* argv[]){
 	using namespace std;
-	string fName;
-	getline(cin,fName);
-	ifstream stm(fName.c_str(),ios::in);
+	ifstream stm(argv[1],ios::in);
 	string x;
 	hamMap M;
 	int t=0;
@@ -29,8 +27,8 @@ int main(void){
 			tkns.push_back(x1);
 		simHashpp hasher(tkns);
 		auto v=M.find(hasher.getHash());
-		cout<<"similar to ["<<t<<"]:"<<endl;
-		copy(v.begin(),v.end(),ostream_iterator<string>(cout,"\n"));
+		cout<<"similar to ["<<t<<"]("<<x<<"):"<<endl<<"\t";
+		copy(v.begin(),v.end(),ostream_iterator<string>(cout,"\n\t"));
 		cout<<"("<<v.size()<<" counts)"<<endl;
 		M.insert(make_pair(hasher.getHash(),x));
 	}
