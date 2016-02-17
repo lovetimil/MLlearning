64966d4d70fc2066ede3842a5f335004e84a346f
Darksun2010 <749881399@qq.com>
2016-02-17 19:32:34 +0800

    faster hammingDist

--- LSH/simHash.c
+++ LSH/simHash.c
@@ -55,12 +55,15 @@ unsigned int simHash_tokens(const char** tokens,int amount){
 	return retNum;
 }
 
+static int bitCount(unsigned int n){
+    n=(n &0x55555555)+((n>>1)&0x55555555);
+    n=(n &0x33333333)+((n>>2)&0x33333333);
+    n=(n &0x0f0f0f0f)+((n>>4)&0x0f0f0f0f);
+    n=(n &0x00ff00ff)+((n>>8)&0x00ff00ff);
+    n=(n &0x0000ffff)+((n>>16)&0x0000ffff);
+    return n;
+}
+
 int hammingDist(unsigned int a,unsigned int b){
-	unsigned x=a^b;
-	int ret=0;
-	while (x){
-		ret++;
-		x&=x-1;
-	}
-	return ret;
+	return bitCount(a^b);
 }
